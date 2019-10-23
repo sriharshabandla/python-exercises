7807edf6116ce3f41645bd767fca51114db89bd3
sriharshabandla <sriharshabandla@gmail.com>
2019-10-23 20:18:15 +0530

    python exercises

---
+++ helloworld.py
@@ -0,0 +1,95 @@
+print('this')
+print('is')
+print('python')
+print('exercises')
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+
+
+x = "apple"
+y = str(4)
+print(x+y)
+
+
+
+print("hello,world")
+print("hello,again")
+print("i like typing this")
+print("this is fun")
+print("yay! printing ")
+print("i'd much rather you 'not'.")
+# print('i'"said"do not touch this.")                           #invalid syntax
+
+#
+
+
+# print 1                                                       #unexpected indent
+
+# a = string
+# print(a)                                                     #NameError: name 'string' is not defined
+
+print("i will now count my chickens:")
+print("hens",25+30/6)
+print("Roosters",100-25*3 % 4)
+print("Now I will count the eggs:",)
+print(3+2+1-5+4 % 2-1/4+6)
+print("is it true that 3+2<5-7?")
+print(3+2 < 5-7)
+print("what is 3 + 2?",3+2)
+print("what is 5-7?",5-7)
+print("oh,that's why it's False.")
+print("how about some more.")
+
+
+#greater than
+print("is it greater?",5 > -2)
+# 5>-2 greater than equal
+print("is it greater? or equal?",5 >= -2)
+# 5<==2 less than or equal
+print("is it less or equal?",5 <= -2)
+
+
+# print("divide a number by zero",30/0)
+#                                                                         ZeroDivisionError: division by zero
+print("divide an integer by floating point",30/6.5)
+print("use // operator instead of / ",30//6.5)
+
+
+
+print(1+2+3 <= 3+1)
+print("hi"+"4")
+print(3==3)
+print("more">"less")
+print("less"<"than")
+print("greater_than" >= "equal_to")
+
+print(2++2)
+print(2--2)
+
+
+minutes = 42
+s = minutes*60
+s1 = 42*s
+print(s1)
+
+
+
+
+k =10
+m = 1.61*k
+print(m)
+
+
+s3 = 105840
+s4 = s3/360
+m1 = 16.1/s4
+print(m1)
+
